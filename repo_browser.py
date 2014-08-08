918947265aed19e1106c3348bb570eb7abf794cc
Varadharajan Mukundan <srinathsmn@gmail.com>
2014-08-08 11:18:39 +0530

    Handle file upload

--- repo_browser.py
+++ repo_browser.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from flask.ext.autoindex import AutoIndexBlueprint
 
+from config import STORAGE_DIR
+
 blueprint = Blueprint('repo_browser', __name__)
-AutoIndexBlueprint(blueprint,'/tmp/storage')
+AutoIndexBlueprint(blueprint,STORAGE_DIR)
--- server.py
+++ server.py
@@ -1,10 +1,24 @@
-from flask import Flask, Blueprint
+import os
+
+from flask import Flask, request, abort
+from werkzeug.utils import secure_filename
 
 import repo_browser
+import helpers
+import config
 
 app = Flask(__name__)
 
 app.register_blueprint(repo_browser.blueprint, url_prefix="/repo")
+
+@app.route("/packages", methods=['POST'])
+def upload_packages():
+    file = request.files['file']
+    if file and helpers.allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(config.STORAGE_DIR, filename))
+        return "Ok"
+    abort(404)
 
 def main():
     app.run(debug=True)
